b2b7653a823c1a71876cc492f359fc6e00661268
darkandrexx <andresherediac@gmail.com>
2015-05-19 17:46:59 +0200

    Arrancamos

--- Pruebagit/src/quepasa.java
+++ Pruebagit/src/quepasa.java
@@ -1,9 +1,5 @@
 
 public class quepasa {
-<<<<<<< HEAD
-	//que pasa winthero!!
-=======
-	//que pasa winthero!!!
-	System.out.println();
->>>>>>> branch 'master' of https://github.com/darkandrexx/Algoritmica.git
+	//Bueno, arrancamos motores con esto del git chavales.
+	
 }
