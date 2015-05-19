dd84cbb6049d6f7db5ef31bef0d92fe00d529fd0
darkandrexx <andresherediac@gmail.com>
2015-05-19 18:34:11 +0200

    Src v2

---
+++ Pruebagit/src/practica4/Main.java
@@ -0,0 +1,77 @@
+package practica4;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Vector;
+
+/**
+ * Clase main que simula la actuacion del robot en la central
+ * 
+ * @author Adrian Herrera Arcila, Andres Heredia Canales y Asier Lopez Uriona
+ * 
+ */
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Central c = new Central("central.txt");
+		c.pintaCentral();
+
+		Robot r = new Robot(c);
+
+		Vector<Casilla> sol = r.getSolucion();
+		Vector<Casilla> mejorSol = r.getMejorSolucion();
+
+		try {
+			System.out.println("Solucion: ");
+			pintaSolucion(sol);
+
+			System.out.println("Mejor solucion: ");
+			pintaSolucion(mejorSol);
+
+			guardaSolucion("UnaSolucion.txt", sol);
+			guardaSolucion("MejorSolucion.txt", mejorSol);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NullPointerException n) {
+			System.out.println("\nNo se han guardado las soluciones");
+		}
+
+	}
+
+	private static void guardaSolucion(String nombreArchivo, Vector<Casilla> v)
+			throws IOException {
+		String str;
+		FileWriter fstream;
+		BufferedWriter out;
+
+		fstream = new FileWriter(nombreArchivo);
+		out = new BufferedWriter(fstream);
+
+		out.append("{\n");
+		for (Casilla c : v) {
+
+			str = "(" + c.getX() + ", " + c.getY() + ")\n";
+			out.append(str);
+
+		}
+		out.append("}");
+		out.close();
+	}
+
+	private static void pintaSolucion(Vector<Casilla> v) {
+		if (v != null) {
+			System.out.println("{");
+			for (Casilla cas : v) {
+				System.out.println("(" + cas.getX() + ", " + cas.getY() + ")");
+			}
+			System.out.println("}");
+		} else {
+			System.out.println("No tiene solucion.");
+		}
+	}
+}
---
+++ Pruebagit/src/practica4/StackIF.java
@@ -0,0 +1,38 @@
+package practica4;
+
+/**
+ * Interfaz de una pila
+ * @author Adrian Herrera Arcila, Andres Heredia Canales y Asier Lopez Uriona
+ *
+ * @param <T>
+ */
+
+public interface StackIF <T>
+{
+	/**
+	 * Devuelve la cima de la pila.
+	 * @return la cima de la pila.
+	 */
+	public T getTop ();
+	/**
+	 * Inserta un elemento a la pida.
+	 * @param element El elemento a ser a?adido.
+	 * @return la pila incluyendo el elemento.
+	 */
+	public StackIF<T> push (T element);
+	/**
+	 * Extrae de la pila el elemento en la cima.
+	 * @return la pila excluyendo la cabeza.
+	 */
+	public StackIF<T> pop ();
+	/**
+	 * Devuelve cierto si la pila esta vacia.
+	 * @return cierto si la pila esta vacia.
+	 */
+	public boolean isEmpty ();
+
+
+	public int hashCode ();
+	public String toString(); 
+
+}
