cb7e15d0c019ddfc3d1ebc1329245dcc3f3fda40
adriwork <adrian.herrera@tutanota.com>
2015-06-08 14:05:51 +0200

    Finalizada y comentada la clase Casilla.java

--- Pruebagit/src/practica4/Casilla.java
+++ Pruebagit/src/practica4/Casilla.java
@@ -15,10 +15,7 @@ public class Casilla
 	//Contenido: libre u ocupada (L/O).
 	private char contenido;
 
-	/**
-	 * Constructor de la clase casilla. Necesita obligatoriamente las
-	 * coordenadas que representa asi como su contenido.
-	 */
+	/** Constructor de la clase casilla; necesita obligatoriamente las coordenadas que representa asi como su contenido. */
 	public Casilla(int x, int y, char p)
 	{
 		this.x = x;
@@ -26,60 +23,52 @@ public class Casilla
 		this.contenido = p;
 	}
 
-	/**
-	 * Devuelve la coordenada X de la casilla.
-	 */
+	/** Devuelve la coordenada X de la casilla. */
 	public int getX()
 	{
 		return this.x;
 	}
 
-	/**
-	 * Devuelve la coordenada Y de la casilla.
-	 */
+	/** Devuelve la coordenada Y de la casilla. */
 	public int getY()
 	{
 		return this.y;
 	}
 
-	/**
-	 * Devuelve el contenido de la casilla.
-	 */
+	/** Devuelve el contenido de la casilla. */
 	public char getContenido()
 	{
 		return this.contenido;
 	}
 
-	/**
-	 * Establece un nuevo contenido para la casilla.
-	 */
+	/** Establece un nuevo contenido para la casilla. */
 	public void setContenido(char p)
 	{
 		this.contenido = p;
 	}
 
-	/* Metodo para establecer la cota de la casilla en un contexto en el que pertenezca a una central */
+	/** Metodo para establecer la cota de la casilla en un contexto en el que pertenezca a una central. */
 	public void setCota(int cota)
 	{
 		this.cota=cota;
 	}
 
-	/* Metodo para obtener la cota */
+	/** Metodo para obtener la cota. */
 	public int getCota()
 	{
 		return this.cota;
 	}
-
+	
+	@Override
+	/** Metodo para comparar casillas respecto a su cota. */
 	public int compareTo(Object o)
 	{
 		Casilla c=(Casilla) o;
 		return (this.cota-c.getCota());
 	}
 
-	/**
-	 * Metodo para comparar dos casillas.
-	 */
 	@Override
+	/** Metodo para comparar dos casillas. */
 	public boolean equals(Object obj)
 	{
 		Casilla c = (Casilla) obj;
